60af161d5849ea7163c487c217c3889144708069
Jason Witting <jason.witting@MacBook-Pro-5.local>
2016-10-18 14:56:23 +0100

    Updated Logging and params

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-	"log"
 	"strconv"
 	"time"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
@@ -14,42 +14,64 @@ var port = flag.String("port", "11211", "memcached port")
 var num = flag.Int("conn_num", 10, "concurrent connections")
 var testduration = flag.Int("duration", 10, "time to run test")
 var timeout = flag.Int("timeout", 100, "client timeout in millisecond's")
+var succesful int
+var failure int
 
 func main() {
 
 	flag.Parse()
+	log.SetLevel(log.InfoLevel)
 
 	for count := *num; count > 0; count-- {
 		s := strconv.Itoa(count)
-		log.Printf("Spinning up goroutine %s", s)
 
-		go connectMemcache("localhost", "11211", "foo"+s, s)
+		log.WithFields(log.Fields{
+			"Routine Number": count,
+			"Key":            "foo" + s,
+			"Value":          s,
+		}).Info("Spinning off goroutine")
+
+		go connectMemcache(*host, *port, "foo"+s, s)
 
 		time.Sleep(50 * time.Millisecond)
 	}
 
-	log.Printf("Spinning up goroutine %s", "FINISHED")
-
 	time.Sleep(time.Duration(*testduration) * time.Second)
+
+	log.Printf("Waiting for %vs", *testduration)
+
+	log.WithFields(log.Fields{
+		"Conn Num":        *num,
+		"Duration":        *testduration,
+		"Successful Gets": succesful,
+		"Failed Gets":     failure,
+	}).Info("Finished Test")
 }
 
 func connectMemcache(host string, port string, key string, value string) {
-	log.Printf("Process(%s) Created Connection: %s with value %s", value, host, port)
+	log.WithFields(log.Fields{
+		"Host":    host,
+		"Port":    port,
+		"Key":     key,
+		"Value":   value,
+		"Timeout": *timeout,
+	}).Info("Created new memcache connection")
 
 	mc := memcache.New(host + ":" + port)
 	mc.Timeout = time.Duration(*timeout) * time.Millisecond
 
 	for i := 0; ; i++ {
-
 		mc.Set(&memcache.Item{Key: key, Value: []byte(value)})
-		log.Printf("Process(%s) Searching for key: %s", value, key)
 		_, err := mc.Get(key)
 
 		if err != nil {
-			log.Printf("Process(%s) ERROR: Could not find key: %v ", value, err)
+			log.WithFields(log.Fields{
+				"Error": err,
+			}).Error("Could not get key")
+			failure++
 			err = nil //reset error
 		} else {
-			log.Printf("Process(%s) Found Key: %s with value: %s ..sleep %d seconds", value, key, value, *timeout/1000)
+			succesful++
 		}
 
 		time.Sleep(1 * time.Second)
